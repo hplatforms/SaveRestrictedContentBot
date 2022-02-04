af9f8f87cbadcd390f7d2b19cd27ca009d6459c0
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-04 16:59:05 +0300

    Update _start.py

--- main/plugins/_start.py
+++ main/plugins/_start.py
@@ -20,40 +20,40 @@ async def sett(event):
     msg = await button.get_reply_message() 
     await event.delete()
     async with Drone.conversation(event.chat_id) as conv: 
-        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        xx = await conv.send_message("Bu Mesaja 'Yanıt' Olarak Küçük Resim İçin Bana Herhangi Bir Resim Gönder.")
         x = await conv.get_reply()
         if not x.media:
-            xx.edit("No media found.")
+            xx.edit("Medya Bulunamadı.")
         mime = x.file.mime_type
         if not 'png' in mime:
             if not 'jpg' in mime:
                 if not 'jpeg' in mime:
-                    return await xx.edit("No image found.")
+                    return await xx.edit("Resim Bulunamadı.")
         await xx.delete()
-        t = await event.client.send_message(event.chat_id, 'Trying.')
+        t = await event.client.send_message(event.chat_id, 'Deniyor.')
         path = await event.client.download_media(x.media)
         if os.path.exists(f'{event.sender_id}.jpg'):
             os.remove(f'{event.sender_id}.jpg')
         os.rename(path, f'./{event.sender_id}.jpg')
-        await t.edit("Temporary thumbnail saved!")
+        await t.edit("Thumbnail Kaydedildi!")
         
 @Drone.on(events.callbackquery.CallbackQuery(data="rem"))
 async def remt(event):  
     Drone = event.client            
-    await event.edit('Trying.')
+    await event.edit('Deniyor.')
     try:
         os.remove(f'{event.sender_id}.jpg')
-        await event.edit('Removed!')
+        await event.edit('Kaldırıldı!')
     except Exception:
-        await event.edit("No thumbnail saved.")                        
+        await event.edit("Küçük Resim Kaydedilmedi.")                        
   
 @Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
     await event.reply(st, 
                       buttons=[
-                              [Button.inline("Kapak ayarla.", data="set"),
-                               Button.inline("kapak kaldır.", data="rem")],
-                              [Button.url("Geliştirici", url="t.me/MaheshChauhan")]])
+                              [Button.inline("Kapak Ayarla.", data="set"),
+                               Button.inline("kapak Kaldır.", data="rem")],
+                              [Button.url("Geliştirici", url="t.me/trbotlistesi")]])
                               
     
     
